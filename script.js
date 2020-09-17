1547993c62e689c762b0fa524922ff324b655c41
Jake Ehmann <jakeehmann@Jakes-MacBook-Air.local>
2020-09-17 12:59:31 -0700

    Created half the for loop(contains Time div, and Row div.

--- script.js
+++ script.js
@@ -5,18 +5,40 @@ $("#currentDay").text(currentday);
 
 
 //Make an array of business hours in order to grab
-
+let businessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"] 
 
 // Get the current hour
+let currentHour = parseInt(moment().format("H"));
+console.log(currentHour);
 
 
 // Test time condition to change color of the div
+for (i = 9; i <= 17; i++) {
+
+    //create row div inside loop, and add attrs and write to DOM (remember to append)
+    let rowDiv = $("<div>");
+    
+    rowDiv.addClass("row time-block");
+
+    $(".container").append(rowDiv);
 
 
-// Create Row Div and add attributes and write the row to the DOM
 
 
-// Create time div and add attributes and write to the Row Div
+    //Create time div inside loop, add attrs and write to DOM (append)
+    let timeDiv = $("<div>");
+    
+    timeDiv.addClass("hour col");
+
+    
+    let hour = $("<h3>");
+    
+    hour.text(businessHours[i-9]);
+    
+    timeDiv.append(hour);
+
+
+}
 
 
 // Create content div and add attributes and write to the row div
